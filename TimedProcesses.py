e401bb754fa9aff65e0aa61ef76f4e82bbd6888a
JonathonW <gandalf72487@yahoo.com>
2011-04-06 22:47:33 -0500

    Add Zip and Unzip processes; remove moore process (it doesn't exist in the timed model of computation)

--- TimedProcesses.py
+++ TimedProcesses.py
@@ -6,6 +6,8 @@ class MealyT(Processes.MealyProcess):
   def __init__(self, partitionFunction, outputFunction, nextStateFunction, initialState, inputSignal, outputSignal):
     Processes.MealyProcess.__init__(self, partitionFunction, outputFunction, nextStateFunction, initialState, inputSignal, outputSignal)
 
-class MooreT(MealyT):
-  def __init__(self, partitionFunction, outputFunction, nextStateFunction, initialState, inputSignal, outputSignal):
-    MealyT.__init__(self, partitionFunction, outputFunction, nextStateFunction, initialState, inputSignal, outputSignal)
+class ZipT(Processes.ZipProcess):
+  pass
+
+class UnzipT(Process.UnzipProcess):
+  pass
