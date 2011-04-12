667e5a9ea835bbe61c13ecb5b5f7d75163662547
unknown <Jonathon@.(none)>
2011-04-12 16:54:06 -0500

    Add global symbols that weren't defined by defaulto

--- utilities.py
+++ utilities.py
@@ -2,6 +2,25 @@
 
 #From http://code.activestate.com/recipes/550804-create-a-restricted-python-function-from-a-string/
 #except we don't restrict input
+# The list of symbols that are included by default in the generated
+# function's environment
+SAFE_SYMBOLS = ["list", "dict", "tuple", "set", "long", "float", "object",
+                "bool", "callable", "True", "False", "dir",
+                "frozenset", "getattr", "hasattr", "abs", "cmp", "complex",
+                "divmod", "id", "pow", "round", "slice", "vars",
+                "hash", "hex", "int", "isinstance", "issubclass", "len",
+                "map", "filter", "max", "min", "oct", "chr", "ord", "range",
+                "reduce", "repr", "str", "type", "zip", "xrange", "None",
+                "Exception", "KeyboardInterrupt"]
+# Also add the standard exceptions
+__bi = __builtins__
+if type(__bi) is not dict:
+    __bi = __bi.__dict__
+for k in __bi:
+    if k.endswith("Error") or k.endswith("Warning"):
+        SAFE_SYMBOLS.append(k)
+del __bi
+
 def stringToFunction(sourceCode, args):
   s = "def __TheFunction__(%s):\n" % args
   s += "\t" + "\n\t".join(sourceCode.split('\n')) + "\n"
@@ -28,6 +47,27 @@ def stringToFunction(sourceCode, args):
   else:
     bi_dict = __builtins__.__dict__
     
+    # Include the safe symbols
+  for k in SAFE_SYMBOLS:
+    # try from current locals
+    try:
+      locs[k] = locals()[k]
+      continue
+    except KeyError:
+      pass
+    # Try from globals
+    try:
+      globs[k] = globals()[k]
+      continue
+    except KeyError:
+      pass
+    # Try from builtins
+    try:
+      bis[k] = bi_dict[k]
+    except KeyError:
+      # Symbol not available anywhere: silently ignored
+      pass
+
   eval(byteCode, globs, locs)
   # As a result, the function is defined as the item __TheFunction__
   # in the locals dictionary
