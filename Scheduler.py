25a7a34113c3ea7d7bf85fdfa7b745ba945bc85a
Jonathon Williams <jwilliams@dhcp-129-59-140-78.n1.vanderbilt.edu>
2011-04-13 16:33:39 -0500

    Add stub scheduler class

--- Scheduler.py
+++ Scheduler.py
@@ -18,3 +18,17 @@ class ProcessContainer:
     
   def getChildren(self):
     return self._childrenList
+    
+class Scheduler:
+  '''
+  Scheduler for processes.  By the time the process graph reaches here, we
+  should have a directed acyclic graph of processes (feedback loops should
+  already be resolved).  Head is an empty ProcessContainer (process assigned to
+  None), whose children are the processes which only take input from our input
+  CSV file.
+  '''
+  def __init__(self, head):
+    pass
+  
+  def runModel(self):
+    pass
