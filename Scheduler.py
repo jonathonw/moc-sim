4a9783a65a81df5b75137c3835e7eaea732c9a25
Jonathon Williams <gandalf72487@yahoo.com>
2011-04-13 17:13:15 -0500

    Topologically sort processes to decide what order to run things in

--- Scheduler.py
+++ Scheduler.py
@@ -1,41 +1,27 @@
 #!/usr/bin/env python
 
+import utilities
 from Processes import Process
 
-class ProcessContainer:
-  '''
-  XML parser (or model checker) will create a bunch of these, connect them in a
-  directed graph corresponding to their dependencies (if a process's output is
-  connected to another process's input, its ProcessContainer should be in this
-  process's children), and pass that to the scheduler.
-  '''
-  def __init__(self, process, parentList, childrenList):
-    self._process = process
-    self._parentList = parentList
-    self._childrenList = childrenList
-    
-  def getProcess(self):
-    return self._process
-    
-  def getChildren(self):
-    return self._childrenList
-    
+
 class Scheduler:
   '''
   Scheduler for processes.  By the time the process graph reaches here, we
   should have a directed acyclic graph of processes (feedback loops should
-  already be resolved).  Head is an empty ProcessContainer (process assigned to
-  None), whose children are the processes which only take input from our input
-  CSV file.  Likewise, tail is an empty ProcessContainer (process assigned to
-  None), whose parents are the processes which don't have their output connected
-  to anything.
+  already be resolved).  Processes is the list of processes (in any arbitrary
+  order; it'll be sorted by us); processDependencies is a list of pairs
+  expressing dependencies between processes (the output of the first process
+  is connected to the input of the second process).
   '''
-  def __init__(self, head, tail):
-    pass
+  def __init__(self, processes, processDependencies, inputSignals, outputSignals):
+    self._processes = utilities.topological_sort(processes, processDependencies)
   
   def runModel(self):
     pass
   
-  def sortProcesses(self):
-  
-  
+  def runOneStep(self):
+    for p in self._processes:
+      #need to catch an out of range exception (or something like that) to determine
+      #when something doesn't have enough input
+      p.runOneStep()
+      
